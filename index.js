b3931e76c912ca9d62c0e802ab81581ea27c500f
Qasim Abdullah <qasimabdullah404@gmail.com>
2020-12-30 14:01:13 +0500

    Refactored

--- index.js
+++ index.js
@@ -9,6 +9,4 @@ const PORT = 5000;
 app.use(bodyParser.json());
 app.use('/users', userRoutes);
 
-app.get('/', (req, res) => res.send('Hello from Express.'));
-
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
